6bb9a77f081309d4e90a2eacf14d51f200b83abf
Gabriella <untitledxx@abv.bg>
2017-09-07 16:04:27 +0300

    PHP AnimeList, JavaScript AnimeList, Java AnimeList, CSharp AnimeList

---
+++ Java Solution/src/main/java/animelist/bindingModel/AnimeBindingModel.java
@@ -0,0 +1,58 @@
+package animelist.bindingModel;
+
+public class AnimeBindingModel {
+    private int id;
+
+    private Integer rating;
+
+    private String name;
+
+    private String description;
+
+    private String watched;
+
+
+    public AnimeBindingModel() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getWatched() {
+        return watched;
+    }
+
+    public void setWatched(String watched) {
+        this.watched = watched;
+    }
+}
